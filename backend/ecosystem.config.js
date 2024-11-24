da6b801b8c7bf0df600909a8041b92528e6eb4c4
ioann7 <ioann.chimrov@gmail.com>
2024-11-24 22:59:03 +0300

    add ecosystem.config

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: `.env.deploy` });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/ioann7/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
